8f0243426598266618ef2d952b5d1a3c892ceb8c
javajohnhub <joedw1978@gmail.com>
2017-08-08 17:25:41 -0400

    test

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
-
+app.use(passport.initialize());
 app.use('/', index);
 app.use('/users', users);
 

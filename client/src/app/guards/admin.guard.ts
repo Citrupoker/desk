4d6866fb6a29cdd093712125035274b7e187fa51
javajohnhub <joedw1978@gmail.com>
2017-08-11 12:05:41 -0400

    error

--- client/src/app/guards/admin.guard.ts
+++ client/src/app/guards/admin.guard.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-
-  constructor(private router: Router, private http: Http) { }
+result: Boolean;
+  constructor(private router: Router, private http: Http) {
+    this.http.get('/admin').map((response: Response) =>
+      this.result = response.json());
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    
-    this.http.get('/admin').map((response: Response) => 
-    console.log(response.json()));
 
+    if(this.result){
+      this.router.navigate(['/admin'], { queryParams: { returnUrl: state.url }});
+      return true;
+    }
     // not logged in so redirect to login page with the return url
-    this.router.navigate(['/admin'], { queryParams: { returnUrl: state.url }});
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
     return false;
   }
 }

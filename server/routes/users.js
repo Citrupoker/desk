35a1b1770be519662d75bfb4e417518971efc478
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-08-14 16:53:50 +0000

    annoying

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,9 +7,11 @@ var User = require('../models/User');
 router.post('/register', passport.authenticate('local-register'), (req, res) => {
     res.json({ status: 1 });
 });
+
 router.post('/login', passport.authenticate('local-login'), (req, res) => {
     res.json({ status: 1 });
 });
+
 router.get('/logout', (req, res) => {
     req.logout();
     res.json({ status: 1 });
